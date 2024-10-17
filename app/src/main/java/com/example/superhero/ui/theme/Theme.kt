d7a187ed436721e43750d61ba0f1eed0f323b967
Paige Winnett <winnettpaige2@gmail.com>
2024-10-17 16:10:14 -0500

    save 3

--- app/src/main/java/com/example/superhero/ui/theme/Theme.kt
+++ app/src/main/java/com/example/superhero/ui/theme/Theme.kt
@@ -9,7 +9,10 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = primaryDark,
@@ -113,6 +116,7 @@ fun SuperheroTheme(
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
+
     }
 
     MaterialTheme(
